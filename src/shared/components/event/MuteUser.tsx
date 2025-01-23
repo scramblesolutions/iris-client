5782834287c88f44122ddbcbf44475a2f699ff80
Martti Malmi <sirius@iki.fi>
2025-01-23 17:07:43 +0200

    irisdb v

--- src/shared/components/event/MuteUser.tsx
+++ src/shared/components/event/MuteUser.tsx
@@ -44,9 +44,7 @@ function MuteUser({user, setMuting, muteState}: MuteUserProps) {
       const followedUsers = socialGraph().getFollowedByUser(socialGraph().getRoot())
       followedUsers.delete(user)
       event.tags = Array.from(followedUsers).map((pubKey) => ["p", pubKey]) as NDKTag[]
-      await event
-        .publish()
-        .catch((e) => console.warn("Error publishing unfollow event:", e))
+      event.publish().catch((e) => console.warn("Error publishing unfollow event:", e))
     }
 
     muteUser(user)
